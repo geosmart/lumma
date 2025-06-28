b1bf89eceaa6ef85b4b6c896f60da29ff56cf2c6
xtool <geosmart@hotmail.com>
2025-06-28 21:45:38 +0800

    mvp-add style switch

---
+++ lib/config/settings_ui_config.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+/// 全局设置页UI配置，包括字体和按钮大小
+class SettingsUiConfig {
+  static double get titleFontSize => 16;
+  static double get subtitleFontSize => 14;
+  static double get buttonIconSize => 28;
+  static double get listTilePaddingH => 16;
+  static double get listTilePaddingV => 8;
+  static double get cardMarginH => 12;
+  static double get cardMarginV => 6;
+  static double get buttonActionIconSize => 22;
+  static FontWeight get titleFontWeight => FontWeight.w500;
+}
